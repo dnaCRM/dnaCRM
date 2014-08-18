cf87b04a3f64b2e3020efa67f6024e24f80da7a7
Vinicius Sorrentino <inshideru@gmail.com>
2014-08-18 16:55:57 -0300

    Adicionado visualização do último perfil cadastrado na tela principal

--- app/views/Home/start.php
+++ app/views/Home/start.php
@@ -1,34 +1,35 @@
 <div class="row">
-                <div class="col-lg-6">
-                    <h1><?php echo $data['pagetitle']; ?></h1>
-<p class="lead">
-    Olá, <?php echo (isset($data['name'])) ? $data['name'] : ""; ?>!
-</p>
-</div>
-<div class="col-lg-6" style="padding: 15px 15px 0 15px;">
-    <div class="well">
+    <div class="col-lg-6">
+        <h1><?php echo $data['pagetitle']; ?></h1>
 
-        Well, well...
-        <?php
-        $cpf = '073.596.567-61';
-        $cpf = str_replace(array('.','-'), '', $cpf);
-
-        echo '<br>'. $cpf . '<br>';
-        $cpf = 0 + $cpf;
-        echo  $cpf;
-
-        echo '<br>';
-        $d = null;
-        $d = '1';
-        if (!$d) {
-            echo 'Uma coisa';
-        } else {
-            echo 'Outra coisa ' . $d;
-        }
-        ?>
-
+        <p class="lead">
+            Olá, <?php echo (isset($data['name'])) ? $data['name'] : ""; ?>!
+        </p>
     </div>
-</div>
+    <div class="col-lg-6" style="padding: 15px 15px 0 15px;">
+        <div class="well">
+
+            Well, well...
+            <?php
+            $cpf = '073.596.567-61';
+            $cpf = str_replace(array('.', '-'), '', $cpf);
+
+            echo '<br>' . $cpf . '<br>';
+            $cpf = 0 + $cpf;
+            echo $cpf;
+
+            echo '<br>';
+            $d = null;
+            $d = '1';
+            if (!$d) {
+                echo 'Uma coisa';
+            } else {
+                echo 'Outra coisa ' . $d;
+            }
+            ?>
+
+        </div>
+    </div>
 </div>
 
 <div class="row">
@@ -46,10 +47,10 @@
                     <div class="panel-body">
                         <?php
 
-                            $test = new ImageTest;
-                            //$test->lob();
-                            $test->show();
-
+                        $perfil = new PerfilModel();
+                        $ultimo_perfil = $perfil->fullList()[0];
+                        echo '<img src="' . $perfil->getImgFolder() . $ultimo_perfil[$perfil->getPk()] . '.jpg" class="img-circle profilefoto">';
+                        var_dump($ultimo_perfil);
                         ?>
                     </div>
                 </div>
@@ -64,7 +65,13 @@
                 </div>
                 <div id="collapseTwo" class="panel-collapse collapse">
                     <div class="panel-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
+                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
+                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
+                        of them accusamus labore sustainable VHS.
                     </div>
                 </div>
             </div>
@@ -78,7 +85,13 @@
                 </div>
                 <div id="collapseThree" class="panel-collapse collapse">
                     <div class="panel-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
+                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
+                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
+                        of them accusamus labore sustainable VHS.
                     </div>
                 </div>
             </div>
@@ -104,38 +117,33 @@
         </ul>
         <div id="myTabContent" class="tab-content">
             <div class="tab-pane fade" id="home">
-                <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+                <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua,
+                    retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.
+                    Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry
+                    richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american
+                    apparel, butcher voluptate nisi qui.</p>
             </div>
             <div class="tab-pane fade active in" id="profile">
                 <p>
 
-                    <form class="form-horizontal" enctype="multipart/form-data" method="post" action="">
-
-                    <fieldset>
-
-                        <legend>Upload de Imagem PostgreSQL</legend>
-                        <div class="form-group">
-
-                            <label for="img_file" class="col-lg-3 control-label">Foto</label>
-                            <div class="col-lg-6">
-                                <input type="file" class="form-control" id="img_file" name="img_file">
-                            </div>
-                        </div>
-                        <input type="submit" value="Enviar" class="btn btn-primary">
-                    </fieldset>
-                    </form>
-
-                <?php
-                var_dump($_FILES);
-                ?>
+                    Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente
+                    ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw
+                    denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 
                 </p>
             </div>
             <div class="tab-pane fade" id="dropdown1">
-                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.</p>
+                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro
+                    fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer,
+                    iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
+                    Leggings gentrify squid 8-bit cred pitchfork.</p>
             </div>
             <div class="tab-pane fade" id="dropdown2">
-                <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater.</p>
+                <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master
+                    cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party
+                    locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before
+                    they sold out farm-to-table VHS viral locavore cosby sweater.</p>
             </div>
         </div>
     </div>
