e60b57afcbe16f9ca1b88d36576c290792765953
Vinicius Sorrentino <inshideru@gmail.com>
2014-10-05 17:51:04 -0300

    Adicionadas e testadas classes PessoaJuridicaTelefoneDTO e PessoaJuridicaTelefoneDAO

---
+++ app/models/PessoaJuridicaTelefoneDAO.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vinicius
+ * Date: 05/10/14
+ * Time: 17:38
+ */
+
+class PessoaJuridicaTelefoneDAO extends DataAccessObject
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->tabela = 'tb_pj_telefone';
+        $this->primaryKey = 'cd_pj_fone';
+        $this->dataTransfer = 'PessoaJuridicaTelefoneDTO';
+    }
+
+    /**
+     * @param PessoaJuridicaTelefoneDTO $dto
+     * @throws Exception
+     */
+    public function gravar(PessoaJuridicaTelefoneDTO $dto)
+    {
+        if ($dto->getCdPjFone() == '') {
+            if (!$this->insert($dto)) {
+                throw new Exception('Impossível Inserir Telefone');
+            }
+        } else {
+            if (!$this->update($dto)) {
+                throw new Exception('Impossível Atualizar Telefone');
+            }
+        }
+    }
+} 
--- app/views/Home/test.php
+++ app/views/Home/test.php
@@ -1,28 +1,25 @@
 <?php
 
-$obj = new PessoaJuridicaEnderecoDTO();
+$obj = new PessoaJuridicaTelefoneDTO();
 $obj
-    ->setCdCatgEnd(1)
-    ->setCdVlCatgEnd(1)
-    ->setCdPessoaFisica(2)
-    ->setCep('14403-180')
-    ->setRua('Rua Alfredo Tosi')
-    ->setNumero('1821')
-    ->setBairro('Núcleo Alpha')
-    ->setCidade('Franca')
-    ->setCdCatgEstado(1)
-    ->setCdVlCatgEstado(1)
+    ->setCdPessoaJuridica(1)
+    ->setFone('(16) 3344-5566')
+    ->setObservacao('Isto é uma observação. Então observe!')
+    ->setCdCatgFonePj(1)
+    ->setCdVlCatgFonePj(1)
+    ->setCdCatgOperadora(1)
+    ->setCdVlCatgOperadora(1)
     ->setObservacao('Orgplan')
     ->setCdUsuarioCriacao(1)
     ->setDtUsuarioCriacao('now()')
     ->setCdUsuarioAtualiza(2)
     ->setDtUsuarioAtualiza('now()');
 
-$objDao = new PessoaJuridicaEnderecoDAO();
+$objDao = new PessoaJuridicaTelefoneDAO();
 
 /** @var PessoaJuridicaEnderecoDTO $obj */
-$obj = $objDao->getById(2);
-$obj->setObservacao('ORGPLAN PARTICIPAÇÕES LTDA');
+$obj = $objDao->getById(1);
+$obj->setObservacao('Número Desconhecido');
 
 
 $obj = $objDao->gravar($obj);
