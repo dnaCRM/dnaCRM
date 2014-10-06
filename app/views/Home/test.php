4483196e6f486441f562fad50732320db7cb94b9
Vinicius Sorrentino <inshideru@gmail.com>
2014-10-06 10:17:14 -0300

    Funcionando CRUD nas classes Relacionados

--- app/views/Home/test.php
+++ app/views/Home/test.php
@@ -1,27 +1,25 @@
 <?php
 
-$obj = new ServicoAdicionalDTO();
+$obj = new RelacionadosDTO();
 $obj
-    ->setCdOrdemServico(1)
-    ->setCdPessoaFisica(1)
-    ->setCdCatgServico(1)
-    ->setCdVlCatgServico(1)
-    ->setCdSetor(1)
-    ->setCdVaga(1)
-    ->setDtInicio('now()')
-    ->setDtFim('now()')
+    ->setCdPessoaFisica1(2)
+    ->setCdPessoaFisica2(1)
+    ->setCdCatgRelacPf1(1)
+    ->setCdVlCatgRelacPf1(1)
+    ->setCdCatgRelacPf2(1)
+    ->setCdVlCatgRelacPf2(1)
     ->setCdUsuarioCriacao(1)
     ->setDtUsuarioCriacao('now()')
     ->setCdUsuarioAtualiza(2)
     ->setDtUsuarioAtualiza('now()');
 
-$objDao = new ServicoAdicionalDAO();
+$objDao = new RelacionadosDAO();
 
-/** @var ServicoAdicionalDTO $obj */
-$obj = $objDao->getById(1);
-$obj->setDtFim('25/12/2014');
+/** @var RelacionadosDTO $obj */
+//$obj = $objDao->getById(null,2);
+//$obj->setDtFim('25/12/2014');
 
 
-$obj = $objDao->gravar($obj);
+//$obj = $objDao->gravar($obj);
 $obj = $objDao->fullList();
 var_dump($obj);
