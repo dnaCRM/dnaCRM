c5eda6cae62be6527d0ca6561938e50f5cfa4696
Raul Martinez <demartinezraul@gmailcom>
2014-10-07 23:26:31 -0300

    Testando Delete Classe OcorrenciaPessoaFisicaEnvolvida

--- app/views/Home/test.php
+++ app/views/Home/test.php
@@ -12,20 +12,19 @@ $obj
 $objDao = new OcorrenciaPessoaFisicaEnvolvidaDAO();
 
 /** @var ServicoAdicionalDTO $obj */
-$obj = $objDao->getById(2);
-$obj->setDtUsuarioAtualiza('07/10/2014');
+//$obj = $objDao->getById(2);
+//$obj->setDtUsuarioAtualiza('07/10/2014');
 
 /**
  * Deletar id selecionado
  */
-//$obj = $objDao->getById(1);
-//$objDao->delete($obj);
-//var_dump($obj);
+$obj = $objDao->getById(2,2);
+$objDao->delete($obj);
 
-$obj = $objDao->gravar($obj);
+//$obj = $objDao->gravar($obj);
 
 $obj = $objDao->fullList();
-var_dump($obj);
+
 
 /**
  *   Atualizar Foto da Pessoa Fisica
