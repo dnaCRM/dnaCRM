0b3fc981d875df1606972f67ae8bde246804fc7e
demartinezraul <demartinezraul@gmail.com>
2014-10-29 23:55:07 -0200

    Alterando layout de condominio

--- app/views/Condominio/formcondominio.php
+++ app/views/Condominio/formcondominio.php
@@ -1,230 +1,190 @@
+<?php
+$condominio = $data['condominio'];
+$condominio_form = new Condominio();
+$cadastrado = $condominio_form->cadastra($condominio); //Não cadastra na entra pois ainda não tem Token
+
+$id_check = $data['id'];
+
+$token = Token::generate();
+?>
 <div class="row">
-    <div class="col-sm-6">
-        <h1><?php echo $data['pagetitle']; ?></h1>
-
-        <p class="lead">
-            <?php echo (isset($data['pagesubtitle'])) ? $data['pagesubtitle'] : ""; ?>
-        </p>
-    </div>
-    <div class="col-sm-6">
-        <div class="well">
-
-            <p>
-                Alguma coisa!
-            </p>
-
-        </div>
+    <div class="col-sm-12">
+        <h3 class="page-header"><?php echo $data['pagetitle']; ?>
+            <small><?php echo $data['pagesubtitle']; ?></small>
+        </h3>
     </div>
 </div>
 
-<!--Teste de Form-->
 <div class="row">
-<div class="col-sm-6">
-    <div class="well">
-        <?php
-        $condominio = $data['condominio'];
-        $condominio_form = new Condominio();
-        $condominio_form->cadastra($condominio);//Não cadastra na entra pois ainda não tem Token
 
-        if (Session::exists('sucesso_salvar_condominio')) {
-            Session::flash('sucesso_salvar_condominio');
-        }
+<div class="col-md-12">
 
-        ?>
-        <img class="img-circle profilefoto"
-             src="<?php echo $condominio->getImPerfil(); ?>">
+<div>
+<ul class="nav nav-tabs">
+    <li class="active"><a href="#principal" data-toggle="tab">Dados Cadastrais</a></li>
+</ul>
+<div id="TabAdicionais" class="tab-content">
+<div class="tab-pane fade active in" id="principal">
 
-        <form id="condominioform" class="form-horizontal" method="post" action="" enctype="multipart/form-data">
-            <fieldset>
-                <legend>Cadastro</legend>
-                <div class="form-group">
-                    <div class="col-xs-offset-8">
-                        <label for="im_perfil" class="btn btn-default control-label">Escolher Foto</label>
+    <div class="row">
+        <?php if ($cadastrado): ?>
 
+            <div class="jumbotron">
+                <div class="container">
+                    <div class="col-md-4">
 
-                        <input type="file" class="hidden" id="im_perfil" name="im_perfil">
+                        <?php
+
+                        if (file_exists("img/uploads/tb_condominio/{$cadastrado->getCdCondominio()}.jpg")) {
+                            $cadastrado->setImPerfil("img/uploads/tb_condominio/{$cadastrado->getCdCondominio()}.jpg");
+                        } else {
+                            $cadastrado->setImPerfil(ICON_USER);
+                        }
+
+                        ?>
+
+                        <img class="img-circle profilefoto left" src="<?php
+                        echo $cadastrado->getImPerfil();?>">
+                    </div>
+                    <div class="col-md-8">
+                        <h1 class="text-success"><span class="glyphicon glyphicon-arrow-right"></span> Sucesso!</h1>
+
+                        <p>Deseja adicionar outro condominio?
+                        </p>
+
+                        <a href="Condominio/" class="btn btn-info" role="button">
+                            <i class="fa fa-arrow-circle-o-left"></i> Voltar
+                        </a>
+
+                        <a href="PessoaJuridica/formperfil/" class="btn btn-success" role="button">
+                            <i class="fa fa-arrow-circle-o-up"></i> Novo
+                        </a>
+
                     </div>
                 </div>
+            </div>
+        <?php else: ?>
+            <form id="condominioform" class="form-horizontal" method="post" action="" enctype="multipart/form-data">
+                <fieldset>
 
-                <div class="form-group">
-                    <div class="col-sm-12">
-                        <label for="nm_condominio" class="control-label">Nome Condominio</label>
+                    <div class="col-md-2">
 
-                        <input type="text" class="form-control" id="nm_condominio" name="nm_condominio"
-                               value="<?php echo $condominio->getNmCondominio() == '' ? Input::get('nm_condominio') : $condominio->getNmCondominio(); ?>"
-                               placeholder="Nome Condominio"
-                               maxlength="50">
+
+                        <img class="img-circle img-responsive" src="<?php echo $condominio->getImPerfil(); ?>"><br>
+
+                        <div class="form-group col-sm-10">
+                            <div>
+                                <label for="im_perfil" class="btn btn-default">Foto</label>
+
+                                <input type="file" class="hidden" id="im_perfil" name="im_perfil">
+                            </div>
+                        </div>
+
                     </div>
-                </div>
 
-                <div class="form-group">
+                    <div class="col-md-6">
 
-                    <div class="col-sm-12">
-                        <label for="rua" class="control-label">Rua</label>
+                        <div class="form-group">
+                            <div class="col-sm-12 inputGroupContainer">
+                                <label for="nm_condominio" class="control-label">Nome Condominio</label>
 
-
-                        <input type="text" class="form-control" id="rua" name="rua"
-                               value="<?php echo $condominio->getRua() == '' ? Input::get('rua') : $condominio->getRua(); ?>"
-                               placeholder="Rua"
-                               maxlength="50">
-                    </div>
-                </div>
-
-                <div class="form-group">
-
-                    <div class="col-sm-12">
-                        <label for="bairro" class="control-label">Bairro</label>
+                                <input type="text" class="form-control" id="nm_condominio" name="nm_condominio"
+                                       value="<?php echo $condominio->getNmCondominio() == '' ? Input::get('nm_condominio') : $condominio->getNmCondominio(); ?>"
+                                       placeholder="Nome Condominio"
+                                       maxlength="50">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-12 inputGroupContainer">
+                                <label for="rua" class="control-label">Rua</label>
 
 
-                        <input type="text" class="form-control" id="bairro" name="bairro"
-                               value="<?php echo $condominio->getBairro() == '' ? Input::get('bairro') : $condominio->getBairro(); ?>"
-                               placeholder="Bairro"
-                               maxlength="50">
-                    </div>
-                </div>
+                                <input type="text" class="form-control" id="rua" name="rua"
+                                       value="<?php echo $condominio->getRua() == '' ? Input::get('rua') : $condominio->getRua(); ?>"
+                                       placeholder="Rua"
+                                       maxlength="50">
+                            </div>
+                        </div>
 
-                <div class="form-group">
-                    <div class="col-sm-12">
-                        <label for="cidade" class="control-label">Cidade</label>
-
-
-                        <input type="text" class="form-control" id="cidade" name="cidade"
-                               value="<?php echo $condominio->getCidade() == '' ? Input::get('cidade') : $condominio->getCidade(); ?>"
-                               placeholder="Cidade"
-                               maxlength="25">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="col-sm-4 inputGroupContainer">
-                        <label for="cep" class="control-label">Cep</label>
+                        <div class="form-group">
+                            <div class="col-sm-4 inputGroupContainer">
+                                <label for="bairro" class="control-label">Bairro</label>
 
 
-                        <input type="text" class="form-control" id="cep" name="cep"
-                               value="<?php echo $condominio->getCep() == '' ? Input::get('cep') : $condominio->getCep(); ?>"
-                               placeholder="99999-999"
-                               maxlength="9">
-                    </div>
-                    <div class="col-sm-4 inputGroupContainer">
-                        <label for="numero" class="control-label">Nº</label>
+                                <input type="text" class="form-control" id="bairro" name="bairro"
+                                       value="<?php echo $condominio->getBairro() == '' ? Input::get('bairro') : $condominio->getBairro(); ?>"
+                                       placeholder="Bairro"
+                                       maxlength="50">
+                            </div>
+                            <div class="col-sm-4 inputGroupContainer">
+                                <label for="cidade" class="control-label">Cidade</label>
 
-                        <input type="text" class="form-control" id="numero" name="numero"
-                               value="<?php echo $condominio->getNumero() == '' ? Input::get('numero') : $condominio->getNumero(); ?>"
-                               placeholder="99999"
-                               maxlength="5">
-                    </div>
-                    <div class="col-sm-4 inputGroupContainer">
-                        <label for="estado" class="control-label">Estado</label>
 
-                        <select class="form-control" id="estado" name="estado">
-                            <option value="">--</option>
-                            <?php
-                            $condominio->setCdVlCatgEstado($condominio->getCdVlCatgEstado() == '' ? Input::get('estado') : $condominio->getCdVlCatgEstado());
-                            foreach ($data['estado'] as $org) {
-                                if ($org->getCdVlCategoria() == $condominio->getCdVlCatgEstado()) {
-                                    echo '<option value="' . $org->getCdVlCategoria() . '" selected>' . $org->getDescVlCatg() . '</option>';
-                                } else {
-                                    echo '<option value="' . $org->getCdVlCategoria() . ' ">' . $org->getDescVlCatg() . '</option>';
+                                <input type="text" class="form-control" id="cidade" name="cidade"
+                                       value="<?php echo $condominio->getCidade() == '' ? Input::get('cidade') : $condominio->getCidade(); ?>"
+                                       placeholder="Cidade"
+                                       maxlength="25">
+                            </div>
+
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <label for="cep" class="control-label">Cep</label>
+
+
+                                <input type="text" class="form-control" id="cep" name="cep"
+                                       value="<?php echo $condominio->getCep() == '' ? Input::get('cep') : $condominio->getCep(); ?>"
+                                       placeholder="99999-999"
+                                       maxlength="9">
+                            </div>
+                        </div>
+                        <div class="col-sm-4 inputGroupContainer">
+                            <label for="numero" class="control-label">Nº</label>
+
+                            <input type="text" class="form-control" id="numero" name="numero"
+                                   value="<?php echo $condominio->getNumero() == '' ? Input::get('numero') : $condominio->getNumero(); ?>"
+                                   placeholder="99999"
+                                   maxlength="5">
+                        </div>
+                        <div class="col-sm-4 inputGroupContainer">
+                            <label for="estado" class="control-label">Estado</label>
+
+                            <select class="form-control" id="estado" name="estado">
+                                <option value="">--</option>
+                                <?php
+                                $condominio->setCdVlCatgEstado($condominio->getCdVlCatgEstado() == '' ? Input::get('estado') : $condominio->getCdVlCatgEstado());
+                                foreach ($data['estado'] as $org) {
+                                    if ($org->getCdVlCategoria() == $condominio->getCdVlCatgEstado()) {
+                                        echo '<option value="' . $org->getCdVlCategoria() . '" selected>' . $org->getDescVlCatg() . '</option>';
+                                    } else {
+                                        echo '<option value="' . $org->getCdVlCategoria() . ' ">' . $org->getDescVlCatg() . '</option>';
+                                    }
                                 }
-                            }
-                            ?>
-                        </select>
+                                ?>
+                            </select>
+                        </div>
                     </div>
-                </div>
+                    <div class="col-sm-offset-4">
 
+                        <input type="hidden" name="cd_condominio" value="<?php echo $data['id']; ?>">
+                        <input type="hidden" name="token" value="<?php echo $token; ?>">
 
-                <input type="hidden" name="cd_condominio" value="<?php echo $data['id']; ?>">
-                <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+                        <div class="form-group">
+                            <div class="col-sm-12 clearfix">
+                                <a href="condominio/visualizar/<?php echo $data['id']; ?>" id="cancel" class="btn btn-default"><span
+                                        class="fa fa-undo"></span> Cancelar</a>
+                                <a href="Condominio/formcondominio" id="novo" class="btn btn-success"><span class="fa fa-file"></span>
+                                    Novo</a>
+                                <button type="submit" name="cadastrar" class="btn btn-primary"><span class="fa fa-check"></span>
+                                    Salvar
+                                </button>
+                            </div>
+                        </div>
 
-                <div class="form-group ">
-                    <div class="col-sm-12">
-                        <a href="Condomino/visualizar/<?php echo $data['id']; ?>="limpar" class="btn btn-default"><span class="fa fa-undo"></span> Cancelar</a>
-                        <button type="reset" name="cancelar" class="btn btn-info"><span class="fa fa-recycle"></span> Limpar</button>
-                        <a href="Condominio/formcondominio" id="novo" class="btn btn-success"><span class="fa fa-file"></span> Novo</a>
-                        <button type="submit" name="cadastrar" class="btn btn-primary"><span class="fa fa-check"></span> Salvar</button>
                     </div>
-                </div>
-            </fieldset>
-        </form>
-
+                </fieldset>
+            </form>
+        <?php endif; ?>
     </div>
-</div>
 
-<div class="col-sm-6">
-
-    <div class="panel-group" id="accordion">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                        $_POST
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseOne" class="panel-collapse collapse in">
-                <div class="panel-body">
-                    <?php
-
-                    var_dump($_POST);
-
-                    ?>
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                        $_SESSION
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse">
-                <div class="panel-body">
-                    <?php
-
-                    var_dump($_SESSION);
-
-                    ?>
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                        <i class="glyphicon glyphicon-leaf"></i> $_FILES
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseThree" class="panel-collapse collapse">
-                <div class="panel-body">
-                    <?php
-
-                    var_dump($_FILES);
-
-                    ?>
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
-                        <i class="glyphicon glyphicon-user"></i> $condominio
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseFour" class="panel-collapse collapse">
-                <div class="panel-body">
-                    <?php
-
-                    var_dump($condominio);
-
-                    ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 </div>
