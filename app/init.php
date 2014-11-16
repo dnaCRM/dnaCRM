906e97574cb3207cd6295a840966b736af578abb
Vinicius Sorrentino <inshideru@gmail.com>
2014-11-16 21:44:26 -0200

    Ajustes

--- app/init.php
+++ app/init.php
@@ -98,7 +98,7 @@ function CodeFail($numero, $mensagem, $arquivo, $linha)
             break;
     }
 
-    //$mensagem = ExceptionMsg::msg($mensagem);
+    $mensagem = ExceptionMsg::msg($mensagem);
     $msg = "<div class=\"alert alert-dismissable alert-{$classecss}\">"
         . '<button type="button" class="close" data-dismiss="alert">×</button>'
         . '<strong>ALERTA! </strong><br>'
@@ -111,7 +111,7 @@ function CodeFail($numero, $mensagem, $arquivo, $linha)
             . "<small>Erro número {$numero}</small>"
             .'</div>';
 
-    echo $erro;
+    echo $msg;
 
 }
 
